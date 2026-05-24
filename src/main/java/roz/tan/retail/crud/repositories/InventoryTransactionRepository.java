5ac3c5a0331ab3d589110a34bb477d05aec518aa
Rozenal Kirill <rosendal_ka@mail.ru>
2026-05-25 00:24:23 +0500

    Исправил длинну строки в InventoryTransactionRepository

--- src/main/java/roz/tan/retail/crud/repositories/InventoryTransactionRepository.java
+++ src/main/java/roz/tan/retail/crud/repositories/InventoryTransactionRepository.java
@@ -3,5 +3,6 @@ package roz.tan.retail.crud.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import roz.tan.retail.models.InventoryTransaction;
 
-public interface InventoryTransactionRepository extends JpaRepository<InventoryTransaction, Long> {
+public interface InventoryTransactionRepository
+        extends JpaRepository<InventoryTransaction, Long> {
 }
