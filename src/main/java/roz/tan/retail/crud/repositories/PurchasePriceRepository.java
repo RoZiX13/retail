2b58295062750a79d5634c17c614c4c45c5c780f
Rozenal Kirill <rosendal_ka@mail.ru>
2026-05-25 00:30:23 +0500

    Исправил ошибку с длинной строки в PurchasePriceRepository

--- src/main/java/roz/tan/retail/crud/repositories/PurchasePriceRepository.java
+++ src/main/java/roz/tan/retail/crud/repositories/PurchasePriceRepository.java
@@ -3,5 +3,6 @@ package roz.tan.retail.crud.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import roz.tan.retail.models.PurchasePrice;
 
-public interface PurchasePriceRepository extends JpaRepository<PurchasePrice, Integer> {
+public interface PurchasePriceRepository
+        extends JpaRepository<PurchasePrice, Integer> {
 }
